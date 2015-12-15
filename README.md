6ae462f1cc547817c09b3fbc09f82d08294e7898
Jibby Ayo-Ani <osa@andrew.cmu.edu>
2015-12-14 23:41:51 -0500

    Read me file added

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+Synopsis
 
-http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
+In this project I implement a distributed application that allows a Carnegie Mellon student to input the courses that they are taking and share that with other people. Potentially this will allow for people other than administrators to see your course schedule and any time it updates they can have a link for your schedule. This utilizes the Scotty Labs Course API:  https://github.com/ScottyLabs/course-api
+
+Motivation
+
+This project was created from an assignment from the Carnegie Mellon undergraduate course Mobile to Cloud: Distributed Applications
+
+Installation
+
+In package.json ensure that you npm install all packages. 
+
+
+License
+
+MIT License
--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {    // do once original document loaded and ready
 
-	
+
 		//ADD COURSE TO SCHEDULE
         $('#addToSchedule').click(function(event) {
         	var course = $('#courseinput').text();
@@ -11,33 +11,13 @@ $(document).ready(function() {    // do once original document loaded and ready
         	var time= $('#timeinput').text();
         	// event.preventDefault()
          	 // $.getJSON('../courses1.json',function(data) {
-					localStorage.setItem("myschedule", ("course: "+ course + ", " + "lecture :" + lecture + ", " + "time :" + time));
-        	  document.getElementById("output2").innerHTML = localStorage.getItem("myschedule");
+					localStorage.setItem("myschedule",
+					 ("course: "+ course + ", " + "lecture :" + lecture +
+					  ", " + "time :" + time));
+        	  document.getElementById("output2").innerHTML 
+        	  = localStorage.getItem("myschedule");
 
-			//	})
-        	
-
-        	//pull variables from the form
-    //     	var course = $('#course').val();
-    //     	var lecture =  $('#lecture').val();
-
-				// $.ajax({
-				//     url: '/' + course + '/' + lecture,
-				//     type: 'PUT',
-				//     data: {
-				//     	"course": course,
-				//     	"lecture": lecture
-				//     },
-				//     success: function(result) {
-  		// 			console.log("success")
-  		// 			$('#output2').html("<p> The course"+ course + "," + lecture +
-				//     		" has been added to your schedule</p> ");
-		   
-				//      },
-				//     error: function(jqxhr){
-				//     	console.log(jqxhr)
-				//     }
-				// 	});
+	
 		});
 
         //GET Requesting course info
@@ -47,11 +27,6 @@ $(document).ready(function() {    // do once original document loaded and ready
         	var course = $('#coursenum').val();
         	var lecture = $('#lecture').val();
         	var time = $('#time').val();
-        	console.log("time", time)
-
-
-        	// somehow have my time show as well 
-
 				$.ajax({
 				    url: '/' + course + '/' + lecture,
 				    type: 'GET',
@@ -83,98 +58,7 @@ $(document).ready(function() {    // do once original document loaded and ready
 				})
 		});
 
-		//POST request update
-		$('#f3').on( "click", 'button',function(event) {
-        	//pull variables from table
-        	
-			var name = $('#name3').val();
-         	var place =  $('#place3').val();
-        	var programConnection = $('#pConn').val();
 
-				$.ajax({
-				    url: '/brigade' ,
-				    type: 'POST',
-				    data: {
-				    	"name": name,
-				    	"place": place,
-				    	"programConnection": programConnection
-				    },
-				    success: function(result) {
-				  		$('#output3').html("<p> The brigade "+ name +
-				    		" is now in "+place + " which is now in conjuction with " +
-				    		 programConnection+"</p>");
-				      },
-				      error: function(jqxhr){
-				    	console.log(jqxhr)
-				    }	
-				    });
-		});
-
-		// //DELETE Request
-		// $('#deletebrigade').click(function(event) {
-  //       	event.preventDefault()
-  //       	//pull name from table
-  //       	var name = $('#name3').val();
-		// 		$.ajax({
-		// 		    url: '/brigade' ,
-		// 		    type: 'DELETE',
-		// 		    data: {
-		// 		    	"name": name,
-		// 		    },
-		// 		    success: function(result) {
-		// 		    $('#name3').val("");
-		// 		    $('#place3').val("");
-		// 		    $('#pConn').val("");
-		// 		      $('#output3').html("<p>"+result+"</p>");
-		// 		      },
-		// 		      error: function(jqxhr){
-		// 		    	console.log(jqxhr)
-		// 		    }	
-		// 		    });
-
-		// });
-
-		// $('#findTime').click(function(event) {
-		// 	event.preventDefault()
-		// 	$.ajax({
-		// 		url: '/' ,
-
-		// 		console.log("HERE")
-		// 		$('#output3').html("<p> The time that you are not free today includes: "+ "</p>");
-		// 		      }
-		// 	})
-
-
-
-		// });
-
- 		// $('#findTime').click(function(event) {
-		// 	event.preventDefault()
-		// 	$.ajax({
-		// 		url: '/' ,
-
-		// 		console.log("HERE")
-		// 		$('#output3').html("<p> The time that you are not free today includes: "+ "</p>");
-		// 		      }
-		// 	})
-
-
-
-		// });
-
- // $('#findTime').click(function(event) {
-		// 	event.preventDefault()
-		// 	$.ajax({
-		// 		url: '/' ,
-
-		// 		console.log("HERE")
-		// 		$('#output3').html("<p> The time that you are not free today includes: "+ "</p>");
-		// 		      }
-		// 	})
-
-
-
-		// });
 
 
 		
